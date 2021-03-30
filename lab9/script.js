88f6b841a0f56b92d826b429a1f9270711e2fc27
Cuong <hoangquoccuong361@gmail.com>
2021-03-30 22:42:34 +0700

    bai tap buoi 9

---
+++ lab9/script.js
@@ -0,0 +1,111 @@
+let myName = "Quoc Cuong ";
+let myAge = 25;
+let isTrue = true;
+
+//let hi = "hello " + myName;
+//alert(hi);
+
+
+let cuong = {
+    name: "Quoc Cuong",
+    age: 25,
+    isTrue: true,
+    job: "student"
+};
+//alert(cuong.name);
+//alert(cuong["age"]);
+
+//let key = "name";
+//alert(cuong[key]);//alert(cuong["name"]);
+
+//let students = ["ngoc", "linh", "hieu"];
+//alert(students[2]);
+
+/*function greeting(name, age) {
+    alert("hello " + name);
+    alert("how old are you " + age);
+}*/
+
+//greeting("quoc cuong", 29);
+
+/*function sub(a, b) {
+    let result = a - b;
+    return result;
+}*/
+
+//alert(sub(1, 2));
+//let x = sub(1, 2);
+
+/*function cum(a, b) {
+    return a - b;
+}
+console.log(cum(1, 2));*/
+
+/*function chuVi(a, b) {
+    return (a + b) * 2;
+}
+function dienTich(a, b) {
+  return a * b;
+}
+console.log("dien tich " + dienTich(5, 9));
+console.log(chuVi(5, 9));*/
+
+
+
+
+
+// Bài 3 trên lớp
+
+/*function tinhChuVi(x) {
+    let chuVi = x * 2 * 3.14;
+    return chuVi;
+}
+function tinhDienTich(y) {
+  let dienTich = y * y * 3.14;
+  return dienTich;
+}
+console.log(" Diện tích hình tròn: " + tinhDienTich(5));
+console.log(" Chu vi hình tròn: " + tinhChuVi(5));*/
+ 
+// Bài 1 về nhà
+
+/*function tinhDoDaiMm(mm) {
+  let tinhMm = mm * 10;
+  return tinhMm;
+}
+function tinhDoDaiM(m) {
+  let tinhM = m / 100;
+  return tinhM;
+}
+function tinhDoDaiKm(km) {
+  let tinhKm = km / 100000;
+  return tinhKm;
+}
+console.log(" Số cm đổi sang mm được: " + tinhDoDaiMm(3) + "mm");
+console.log(" Số cm đổi sang m được: " + tinhDoDaiM(3) + "m");
+console.log(" Số cm đổi sang km được: " + tinhDoDaiKm(3) + "km");*/
+
+// Bài 2 về nhà
+
+/*function doiSangDoF(f) {
+  let doiDoF = f * 1.8 + 32;
+  return doiDoF;
+}
+function doiSangDoK(k) {
+  let doiDoK = k + 273.15;
+  return doiDoK;
+}
+console.log(" chuyển đổi độ C ra độ F: " + doiSangDoF(5) + " F");
+console.log(" chuyển đổi độ C ra độ K: " + doiSangDoK(5) + " K");*/
+
+//Bài 3 về nhà
+
+/*let s = prompt();
+let h = s / 3600;
+let m = (s % 3600) / 60;
+let se = s % 60;
+console.log(
+  parseInt(h) + "giờ" + ":" + parseInt(m) + "phút" + ":" + se + "giây"
+);
+*/
+
